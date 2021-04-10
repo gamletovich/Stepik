c7d9a404b72b11a5ca5b62fde037e08286220b60
gamletovich <z@fastlive.eu>
2021-04-10 17:51:38 +0200

    module 3 step 3 task 1 completed

--- PythonFundamentalsAndUsage/Module 2 - The standard means of language/Module 3 Step 3 Task 1 - Requests.py
+++ PythonFundamentalsAndUsage/Module 2 - The standard means of language/Module 3 Step 3 Task 1 - Requests.py
@@ -1,5 +1,62 @@
 """
+Рассмотрим два HTML-документа A и B.
+Из A можно перейти в B за один переход, если в A есть ссылка на B, т. е. внутри A есть тег <a href="B">, возможно с дополнительными параметрами внутри тега.
+Из A можно перейти в B за два перехода если существует такой документ C, что из A в C можно перейти за один переход и из C в B можно перейти за один переход.
 
+Вашей программе на вход подаются две строки, содержащие url двух документов A и B.
+Выведите Yes, если из A в B можно перейти за два перехода, иначе выведите No.
+
+Обратите внимание на то, что не все ссылки внутри HTML документа могут вести на существующие HTML документы.
+
+Sample Input 1:
+
+https://stepic.org/media/attachments/lesson/24472/sample0.html
+https://stepic.org/media/attachments/lesson/24472/sample2.html
+Sample Output 1:
+
+Yes
+Sample Input 2:
+
+https://stepic.org/media/attachments/lesson/24472/sample0.html
+https://stepic.org/media/attachments/lesson/24472/sample1.html
+Sample Output 2:
+
+No
+Sample Input 3:
+
+https://stepic.org/media/attachments/lesson/24472/sample1.html
+https://stepic.org/media/attachments/lesson/24472/sample2.html
+Sample Output 3:
+
+Yes
 """
+from lxml import html
 import requests
 
+
+def get_links(page):
+    try:
+        res = requests.get(page)
+        webpage = html.fromstring(res.content)
+        return webpage.xpath('//a/@href')
+    except:
+        return []
+
+
+def two_steps():
+    links1 = get_links(url1)
+
+    for el in links1:
+        links2 = get_links(el)
+        if url2 in links2:
+            return True
+    return False
+
+
+url1 = input()
+url2 = input()
+
+if two_steps():
+    print("Yes")
+else:
+    print("No")
