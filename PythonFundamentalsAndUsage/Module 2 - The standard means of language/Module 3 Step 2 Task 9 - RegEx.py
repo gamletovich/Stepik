80d7c29b07b25dfa8e4b3f2329970b638e5fa086
gamletovich <z@fastlive.eu>
2021-04-10 16:20:20 +0200

    module 3 step 2 task 9 completed

--- PythonFundamentalsAndUsage/Module 2 - The standard means of language/Module 3 Step 2 Task 9 - RegEx.py
+++ PythonFundamentalsAndUsage/Module 2 - The standard means of language/Module 3 Step 2 Task 9 - RegEx.py
@@ -1,3 +1,20 @@
 """
+Вам дана последовательность строк.
+В каждой строке замените все вхождения нескольких одинаковых букв на одну букву.
+Буквой считается символ из группы \w.
+Sample Input:
 
+attraction
+buzzzz
+Sample Output:
+
+atraction
+buz
 """
+import re
+import sys
+
+pattern = r'(\w)\1+'
+
+for line in sys.stdin:
+    print(re.sub(pattern, r'\2\1\3', line.rstrip()))
